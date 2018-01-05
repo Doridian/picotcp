2f0ebee8c4952361c4f800fde7e7563aa3cfc34d
Doridian <mriq91@gmail.com>
2018-01-05 07:51:55 -0800

    formatting

--- emscripten/pre.js
+++ emscripten/pre.js
@@ -221,9 +221,7 @@ Socket.prototype.close = function () {
 	delete Module._sockets[this.fd];
 	Module._pico_socket_close(this.fd);
 	this.fd = undefined;
-	this.wbuffer.forEach(obj => {
-		Module._free(obj.ptr);
-	});
+	this.wbuffer.forEach(obj => Module._free(obj.ptr));
 	this.wbuffer = [];
 };
 Socket.prototype._send = function (ptr, len) {
